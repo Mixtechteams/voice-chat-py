63dae9c15c0f8682a78a50b40be18174dfc8e1db
hheimerd <hheimerd@gmail.com>
2023-03-16 09:57:59 +0300

    fix button

--- modules/com_button_listener.py
+++ modules/com_button_listener.py
@@ -1,19 +1,16 @@
-import binascii
-
 from typing import Callable
-from serial import Serial, SerialException
+import serial
 
 class ComButtonListener:
     def __init__(self, port_name, on_button_state_change: Callable[[bool], None] | None = None) -> None:
-        print()
         self.on_button_state_change = on_button_state_change
-        self.serial = Serial(baudrate=9600, timeout=1000, write_timeout=1000)
+        self.serial = serial.Serial(baudrate=9600, timeout=1000, write_timeout=1000)
         for i in range(5):
             self.serial.port = port_name
 
             try:
                 self.serial.open()
-            except SerialException as e:
+            except serial.SerialException as e:
                 print(f'Error while opening. Attempt {i+1}/5. Base error: {e}')
             else:
                 break
@@ -21,20 +18,15 @@ class ComButtonListener:
         if not self.serial.is_open:
             raise (RuntimeError('Error: could not open port'))
 
+
     def update(self):
-        total_read = 0
-        attempts = 10
+        buffer = bytearray(3)
+        message = bytearray([0xa1, 0, 0x86])  # 100
 
-        while total_read % 3 != 0:
-            r = self.serial.read(3)
-            r = binascii.hexlify(bytearray(r))
-            arr = r[total_read % 3:3 - total_read % 3]
+        # Команда старта
+        self.serial.write(message)
 
-            total_read += len(arr)
-            
-            attempts = attempts - 1
-            if attempts <= 0:
-                break
+        buffer = self.serial.read(3)
 
         if self.on_button_state_change:
-            self.on_button_state_change(arr[1] & 0b10000000 > 0)
+            self.on_button_state_change(buffer[1] & 0b10000000 > 0)
